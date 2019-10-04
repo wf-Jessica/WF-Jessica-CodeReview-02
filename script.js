d42cfd496dd71144bc572bb7469dcad87bb7d965
Jessica Triltsch <triltsch.jessica@hotmail.com>
2019-10-04 14:40:15 +0200

    second CodeReview

---
+++ script.js
@@ -0,0 +1,22 @@
+function calculateInsurance(){
+	var countrys = document.getElementById("Country").value;
+	var insurance;
+	var horse_power= document.getElementById("hp").value;
+	var age = document.getElementById("ag").value;
+	var name = document.getElementById("name").value;
+switch (countrys) {
+	case "0":
+	insurance = 0;
+	break;
+	case "1":
+	insurance = horse_power*100/age +50;
+	break;
+	case "2":
+	insurance = horse_power*120/age +100;
+	break;
+	case "3":
+	insurance = horse_power*150/(age+3)+50;
+	break;
+	}
+	document.getElementById("insur").innerHTML = name +" , your insurance costs " + insurance + " €";
+}
